d51eb083f04deda2f2db369acde8a0d28ced6910
KanterIV <126972573+KanterIV@users.noreply.github.com>
2023-10-08 10:10:06 +0200

    update ContactForm component from class to hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import ContactForm from 'components/ContactForm/ContactForm';
+import { ContactForm } from 'components/ContactForm/ContactForm';
 import ContactList from 'components/ContactList/ContactList';
 import Filter from 'components/Filter/Filter';
 import css from './App.module.css';
